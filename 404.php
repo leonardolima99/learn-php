3113638e54d92cde8e378846b04dc1fc8db96c90
Leonardo Lima Coelho <dev.leonardolima@gmail.com>
2024-03-25 15:25:46 -0300

    init commit

---
+++ 404.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World PHP</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    html, body {
+      width: 100%;
+      height: 100%;
+    }
+    .error-404 {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: end;
+    }
+    .john-bottom-center {
+      height: 75%;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="error-404">
+    <img src="/assets/img/john-travolta-searching.gif" class="john-bottom-center" />
+  </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World PHP</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: sans-serif;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>
+    <?php
+    echo "Olá, mundo! 🔥";
+    $name = "Leonardo";
+    ?>
+  </h1>
+  <p>
+    <?php
+    if ($name > 0) {
+      echo "Meu nome é <strong>{$name}</strong>! 🔥";
+    } else {
+      echo 'Deu ruim?';
+    }
+    ?>
+  </p>
+</body>
+
+</html>
